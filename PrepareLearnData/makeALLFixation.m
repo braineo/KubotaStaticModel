52615e80397808001fcabdc7030459feacf7411e
Ye Binbin <yebinbin@iis.u-tokyo.ac.jp>
2014-01-30 19:02:03 +0900

    judgement on whether file exist

--- PrepareLearnData/makeALLFixation.m
+++ PrepareLearnData/makeALLFixation.m
@@ -2,7 +2,7 @@
 datafolder = '../Data/prefDataCSV/';
 savefile = '..Data/EXPALLFixationsPref.mat';
 datasetSize = 450;
-testSubjectNumber = 12;
+testSubjectNumber = 25;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 EXPALLFixations = {};
@@ -14,9 +14,13 @@ end
 for imgidx = 1:datasetSize
     for subject = 1:testSubjectNumber
         datafile = sprintf('%s%02d%03d.csv', datafolder, subject, imgidx);
-        eyedata = load(datafile);
-        [data,Fix,Sac] = getFixations(eyedata);
-        EXPALLFixations{imgidx}{length(EXPALLFixations{imgidx})+1}=Fix;
+        if(~exist(datafile))
+            continue
+        else
+            eyedata = load(datafile);
+            [data,Fix,Sac] = getFixations(eyedata);
+            EXPALLFixations{imgidx}{length(EXPALLFixations{imgidx})+1}=Fix;
+        end
     end
 end
 
